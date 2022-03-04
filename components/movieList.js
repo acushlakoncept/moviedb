d1d0da57e4e9c8e5e4ae299ccf75463c60d25772
Uduak Essien <acushla4real@gmail.com>
2022-03-04 16:33:06 +0100

    Add movie list

---
+++ components/movieList.js
@@ -0,0 +1,45 @@
+import Link from "next/link";
+import { Card } from "react-bootstrap";
+
+
+const MovieList = ({ movies }) => {
+
+  const shorten = (text, maxLength) => {
+    if (text && text.length > maxLength) {
+      return text.substring(0, maxLength) + "...";
+    } else {
+      return text;
+    }
+  }
+
+  return (
+    <>
+     { movies.map(movie => (
+          <div key={movie.id} className="col-lg-4 col-md-6 mb-4">   
+            <Card className="h-100">
+              <Link href={`/movies/${movie.id}`}>
+                <a><Card.Img variant="top" src={movie.image} /></a>
+              </Link>
+              <Card.Body>
+                <Card.Title> 
+                  <Link href={`/movies/${movie.id}`}>
+                  <a href="#">{movie.name}</a>
+                  </Link>
+                </Card.Title>
+                <Card.Text>
+                  {shorten(movie.description, 100)}
+                </Card.Text>
+              </Card.Body>
+              <Card.Footer>
+                <small className="text-muted">{movie.rating}</small>
+              </Card.Footer>
+            </Card>
+          </div>
+     ))
+
+     }
+    </>
+  )
+}
+
+export default MovieList;
