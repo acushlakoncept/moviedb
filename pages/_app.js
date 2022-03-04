388fd5d4b0e1e4d42f5a1a9e6cbb282803f1da3d
Uduak Essien <acushla4real@gmail.com>
2022-03-04 16:32:23 +0100

    Update app with base layout page

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,26 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBar from '../components/navBar';
+import Footer from '../components/footer';
 import '../styles/globals.css'
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className='base-page'>
+      <Head>
+      <title>Movie DB</title>
+      </Head>
+      <NavBar />
+      <Component {...pageProps} />
+      <Footer />  
+      <style jsx>{`
+        .base-page {
+          padding-top: 80px;
+        }
+      `}</style>
+    </div>
+
+    )
 }
 
 export default MyApp
